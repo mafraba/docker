5a4b4c50a683f05b5f5565539ba68ffedbe9a20c
Nicolas De Loof <nicolas.deloof@gmail.com>
2015-11-13 08:19:14 +0100

    publish script

---
+++ publish.sh
@@ -0,0 +1,9 @@
+JENKINS_SHA=$(curl -u $USER:$PASS https://repo-internal.cloudbees.com/com/cloudbees/jenkins/main/jenkins-enterprise-war/$JENKINS_VERSION/jenkins-enterprise-war-$JENKINS_VERSION.war.sha1)
+
+sed -e "s/ENV JENKINS_VERSION .*/ENV JENKINS_VERSION $JENKINS_VERSION/g" -e "s/ENV JENKINS_SHA .*/ENV JENKINS_SHA $JENKINS_SHA/g" Dockerfile > Dockerfile.$JENKINS_VERSION
+
+docker build -f Dockerfile.$JENKINS_VERSION -t cloudbees/jenkins-enterprise:$JENKINS_VERSION .
+docker push cloudbees/jenkins-enterprise:$JENKINS_VERSION
+
+docker build -f Dockerfile.$JENKINS_VERSION -t cloudbees/jenkins-enterprise:latest .
+docker push cloudbees/jenkins-enterprise:latest
