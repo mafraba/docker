f6b27bac3a8042d94c94d744e7c69d56069be360
Nicolas De loof <nicolas.deloof@gmail.com>
2015-11-13 16:49:56 +0100

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@ JENKINS_SHA=$(curl -u $USER:$PASS https://repo-internal.cloudbees.com/com/cloudb
 
 sed -e "s/ENV JENKINS_VERSION .*/ENV JENKINS_VERSION $JENKINS_VERSION/g" -e "s/ENV JENKINS_SHA .*/ENV JENKINS_SHA $JENKINS_SHA/g" Dockerfile > Dockerfile.$JENKINS_VERSION
 
-docker build -f Dockerfile.$JENKINS_VERSION -t cloudbees/jenkins-enterprise:$JENKINS_VERSION .
+docker build --no-cache -f Dockerfile.$JENKINS_VERSION -t cloudbees/jenkins-enterprise:$JENKINS_VERSION .
 docker push cloudbees/jenkins-enterprise:$JENKINS_VERSION
 
 docker build -f Dockerfile.$JENKINS_VERSION -t cloudbees/jenkins-enterprise:latest .
