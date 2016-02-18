a453b7f8599a0087f94152c35a814b0532aff626
Carlos Sanchez <carlos@apache.org>
2016-02-18 14:26:46 +0100

    Switch publish script to just update and git push
    Build is done at dockerhub

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -ex
+
+if [ "$#" -ne 1 ] || [ -z "${PASSWORD}" ]
+then
+  echo "Usage: PASSWORD=xxx $0 VERSION"
+  exit 1
+fi
+
+JENKINS_VERSION=$1
+JENKINS_SHA=$(curl -sSL -u $USER:$PASSWORD https://repo-internal.cloudbees.com/com/cloudbees/jenkins/main/jenkins-enterprise-war/$JENKINS_VERSION/jenkins-enterprise-war-$JENKINS_VERSION.war.sha1)
+echo $JENKINS_VERSION $JENKINS_SHA
+
+sed -e "s/ENV JENKINS_VERSION .*/ENV JENKINS_VERSION $JENKINS_VERSION/g" -e "s/ENV JENKINS_SHA .*/ENV JENKINS_SHA $JENKINS_SHA/g" Dockerfile > Dockerfile.$JENKINS_VERSION
+mv Dockerfile.$JENKINS_VERSION Dockerfile
+
+docker build -t cloudbees/jenkins-enterprise:$JENKINS_VERSION .
+
+git add Dockerfile
+git commit -m "Update to ${JENKINS_VERSION}"
+git tag cje-$JENKINS_VERSION
+git push --tags
+git push
